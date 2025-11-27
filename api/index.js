2a4e9c5c919a94b3afce0d6e7f056e7ee4edc681
itsimed <ie.belouettar@gmail.com>
2025-11-27 15:57:47 +0100

    Fix: création index.html statique avec redirection meta + JS vers register.html

--- api/index.js
+++
@@ -1,8 +0,0 @@
-// Redirection racine vers register.html
-const { createApp } = require('../app');
-
-module.exports = (req, res) => {
-  // Simple redirection sans passer par Express
-  res.writeHead(302, { Location: '/register.html' });
-  res.end();
-};
