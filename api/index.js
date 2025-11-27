ab305615c80a12c9803dfc2cae209e24d675e002
itsimed <ie.belouettar@gmail.com>
2025-11-27 15:07:12 +0100

    Fix Vercel API routing: rename catch-all to proper syntax and remove redundant index.js

--- api/index.js
+++
@@ -1,9 +0,0 @@
-// Handler serverless pour Vercel: adapter Express à la signature (req, res)
-const { createApp } = require('../app');
-
-// Singleton: réutilise la même instance d'app pour les invocations "warm"
-let app;
-module.exports = (req, res) => {
-  if (!app) app = createApp();
-  return app(req, res);
-};
