69cb84747cfccebfa531de1c9db6b8f0f9ada55b
itsimed <ie.belouettar@gmail.com>
2025-11-27 15:57:33 +0100

    Fix: import createApp dans api/index.js pour cohérence

--- api/index.js
+++ api/index.js
@@ -1,5 +1,8 @@
 // Redirection racine vers register.html
+const { createApp } = require('../app');
+
 module.exports = (req, res) => {
+  // Simple redirection sans passer par Express
   res.writeHead(302, { Location: '/register.html' });
   res.end();
 };
