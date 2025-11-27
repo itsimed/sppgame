3ffb5e2545eab451c293d85ab2a2a28bc171b485
itsimed <ie.belouettar@gmail.com>
2025-11-27 17:07:38 +0100

    Consolidation des routes de vote dans un seul endpoint pour limiter le nombre de functions

--- api/active-vote.js
+++
@@ -1,7 +0,0 @@
-const { createApp } = require('../app');
-
-const app = createApp();
-
-module.exports = async (req, res) => {
-  await app(req, res);
-};
--- api/vote.js
+++ api/vote.js
@@ -1,2 +1,20 @@
 const { createApp } = require('../app');
-module.exports = (req, res) => createApp()(req, res);
+
+const app = createApp();
+
+module.exports = async (req, res) => {
+  // Gérer plusieurs routes de vote dans un seul endpoint
+  const path = req.url.split('?')[0];
+  
+  if (path === '/api/start-vote' || path === '/start-vote') {
+    req.url = '/api/start-vote';
+  } else if (path === '/api/active-vote' || path === '/active-vote') {
+    req.url = '/api/active-vote';
+  } else if (path === '/api/stop-vote' || path === '/stop-vote') {
+    req.url = '/api/stop-vote';
+  } else {
+    req.url = '/api/vote';
+  }
+  
+  await app(req, res);
+};
